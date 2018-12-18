b6965e38df37625c75b329b1b53d90a0cfeeb4d8
JosephSnedeker1 <josephscottsnedeker@gmail.com>
2018-12-17 18:53:50 -0700

    implemented sause buttons

---
+++ Sandwich Builder/Assets/Scripts/OnClickSauses.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class OnClickSauses : MonoBehaviour
+{
+
+    [SerializeField]
+    Sauses sause;
+
+    public void onClick()
+    {
+        if (!GameManager.Instance.selectedSandwich.sauses.Contains(sause))
+        {
+            GameManager.Instance.selectedSandwich.sauses.Add(sause);
+        }
+        else if (GameManager.Instance.selectedSandwich.sauses.Contains(sause))
+        {
+            GameManager.Instance.selectedSandwich.sauses.Remove(sause);
+        }
+
+    }
+}
+
---
+++ Sandwich Builder/Assets/Scripts/SelectingSauses.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SelectingSauses : MonoBehaviour
+{
+
+    [SerializeField]
+    Sauses sause;
+    bool selected;
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.selectedSandwich.sauses.Contains(sause))
+        {
+
+            selected = true;
+        }
+        if (!GameManager.Instance.selectedSandwich.sauses.Contains(sause))
+        {
+            selected = false;
+        }
+        if (selected)
+        {
+            GetComponent<Button>().image.color = new Color(1, 1, 1, 1);
+        }
+        if (!selected)
+        {
+            GetComponent<Button>().image.color = new Color(1, 1, 1, .35f);
+        }
+
+    }
+
+}
