87849f232838826218278606f0f762c781b4e9b6
JosephSnedeker1 <josephscottsnedeker@gmail.com>
2018-12-17 19:41:02 -0700

    made veggies appear on the pay screen

---
+++ Sandwich Builder/Assets/Scripts/veggieText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class veggieText : MonoBehaviour {
+    [SerializeField]
+    int instance;
+	// Use this for initialization
+	void Start () {
+        if (instance < GameManager.Instance.selectedSandwich.veggies.Count)
+        {
+            GetComponent<Text>().text = GameManager.Instance.selectedSandwich.veggies[instance].ToString();
+        }
+        else
+            GetComponent<Text>().text = "";
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
