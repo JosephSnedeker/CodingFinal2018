4453083ee5fd4067d4904780d4605ba6909c9f3d
JosephSnedeker1 <josephscottsnedeker@gmail.com>
2018-12-18 10:49:47 -0700

    Made sause text show up on pay

---
+++ Sandwich Builder/Assets/Scripts/SauseText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SauseText : MonoBehaviour
+{
+    [SerializeField]
+    int instance;
+    // Use this for initialization
+    void Start()
+    {
+        if (instance < GameManager.Instance.selectedSandwich.sauses.Count)
+        {
+            GetComponent<Text>().text = GameManager.Instance.selectedSandwich.sauses[instance].ToString();
+        }
+        else
+            GetComponent<Text>().text = "";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
